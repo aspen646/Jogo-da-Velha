08d3af5afc466ad2338273fbd4bb93a87618bddc
Arthur Henrique <arthur12souza@hotmail.com>
2020-08-20 16:57:22 -0300

    Jogo da velha finalizado v1

---
+++ jogoDaVelhaArthur/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace jogoDaVelhaArthur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a11 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVez = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.a33 = new System.Windows.Forms.Button();
+            this.a32 = new System.Windows.Forms.Button();
+            this.a31 = new System.Windows.Forms.Button();
+            this.a23 = new System.Windows.Forms.Button();
+            this.a22 = new System.Windows.Forms.Button();
+            this.a21 = new System.Windows.Forms.Button();
+            this.a13 = new System.Windows.Forms.Button();
+            this.a12 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPontuacao1 = new System.Windows.Forms.Label();
+            this.lblPontuacao2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // a11
+            // 
+            this.a11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a11.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a11.Location = new System.Drawing.Point(49, 14);
+            this.a11.Name = "a11";
+            this.a11.Size = new System.Drawing.Size(130, 130);
+            this.a11.TabIndex = 0;
+            this.a11.Text = "X";
+            this.a11.UseVisualStyleBackColor = true;
+            this.a11.Click += new System.EventHandler(this.a11_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(298, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "JOGO DA VELHA";
+            // 
+            // lblVez
+            // 
+            this.lblVez.AutoSize = true;
+            this.lblVez.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVez.ForeColor = System.Drawing.Color.Red;
+            this.lblVez.Location = new System.Drawing.Point(324, 37);
+            this.lblVez.Name = "lblVez";
+            this.lblVez.Size = new System.Drawing.Size(150, 19);
+            this.lblVez.TabIndex = 2;
+            this.lblVez.Text = "Vez do Jogador 1 (X)";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
+            this.panel1.Controls.Add(this.a33);
+            this.panel1.Controls.Add(this.a32);
+            this.panel1.Controls.Add(this.a31);
+            this.panel1.Controls.Add(this.a23);
+            this.panel1.Controls.Add(this.a22);
+            this.panel1.Controls.Add(this.a21);
+            this.panel1.Controls.Add(this.a13);
+            this.panel1.Controls.Add(this.a12);
+            this.panel1.Controls.Add(this.a11);
+            this.panel1.Location = new System.Drawing.Point(12, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 430);
+            this.panel1.TabIndex = 3;
+            // 
+            // a33
+            // 
+            this.a33.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a33.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a33.Location = new System.Drawing.Point(321, 279);
+            this.a33.Name = "a33";
+            this.a33.Size = new System.Drawing.Size(130, 130);
+            this.a33.TabIndex = 8;
+            this.a33.Text = "X";
+            this.a33.UseVisualStyleBackColor = true;
+            this.a33.Click += new System.EventHandler(this.a33_Click);
+            // 
+            // a32
+            // 
+            this.a32.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a32.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a32.Location = new System.Drawing.Point(185, 279);
+            this.a32.Name = "a32";
+            this.a32.Size = new System.Drawing.Size(130, 130);
+            this.a32.TabIndex = 7;
+            this.a32.Text = "X";
+            this.a32.UseVisualStyleBackColor = true;
+            this.a32.Click += new System.EventHandler(this.a32_Click);
+            // 
+            // a31
+            // 
+            this.a31.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a31.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a31.Location = new System.Drawing.Point(49, 279);
+            this.a31.Name = "a31";
+            this.a31.Size = new System.Drawing.Size(130, 130);
+            this.a31.TabIndex = 6;
+            this.a31.Text = "X";
+            this.a31.UseVisualStyleBackColor = true;
+            this.a31.Click += new System.EventHandler(this.a31_Click);
+            // 
+            // a23
+            // 
+            this.a23.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a23.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a23.Location = new System.Drawing.Point(321, 146);
+            this.a23.Name = "a23";
+            this.a23.Size = new System.Drawing.Size(130, 130);
+            this.a23.TabIndex = 5;
+            this.a23.Text = "X";
+            this.a23.UseVisualStyleBackColor = true;
+            this.a23.Click += new System.EventHandler(this.a23_Click);
+            // 
+            // a22
+            // 
+            this.a22.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a22.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a22.Location = new System.Drawing.Point(185, 146);
+            this.a22.Name = "a22";
+            this.a22.Size = new System.Drawing.Size(130, 130);
+            this.a22.TabIndex = 4;
+            this.a22.Text = "X";
+            this.a22.UseVisualStyleBackColor = true;
+            this.a22.Click += new System.EventHandler(this.a22_Click);
+            // 
+            // a21
+            // 
+            this.a21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a21.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a21.Location = new System.Drawing.Point(49, 146);
+            this.a21.Name = "a21";
+            this.a21.Size = new System.Drawing.Size(130, 130);
+            this.a21.TabIndex = 3;
+            this.a21.Text = "X";
+            this.a21.UseVisualStyleBackColor = true;
+            this.a21.Click += new System.EventHandler(this.a21_Click);
+            // 
+            // a13
+            // 
+            this.a13.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a13.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a13.Location = new System.Drawing.Point(321, 14);
+            this.a13.Name = "a13";
+            this.a13.Size = new System.Drawing.Size(130, 130);
+            this.a13.TabIndex = 2;
+            this.a13.Text = "X";
+            this.a13.UseVisualStyleBackColor = true;
+            this.a13.Click += new System.EventHandler(this.a13_Click);
+            // 
+            // a12
+            // 
+            this.a12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a12.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a12.Location = new System.Drawing.Point(185, 14);
+            this.a12.Name = "a12";
+            this.a12.Size = new System.Drawing.Size(130, 130);
+            this.a12.TabIndex = 1;
+            this.a12.Text = "X";
+            this.a12.UseVisualStyleBackColor = true;
+            this.a12.Click += new System.EventHandler(this.a12_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(587, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PONTUAÇÃO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(609, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Jogador 1";
+            // 
+            // lblPontuacao1
+            // 
+            this.lblPontuacao1.AutoSize = true;
+            this.lblPontuacao1.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPontuacao1.Location = new System.Drawing.Point(625, 185);
+            this.lblPontuacao1.Name = "lblPontuacao1";
+            this.lblPontuacao1.Size = new System.Drawing.Size(76, 86);
+            this.lblPontuacao1.TabIndex = 6;
+            this.lblPontuacao1.Text = "0";
+            // 
+            // lblPontuacao2
+            // 
+            this.lblPontuacao2.AutoSize = true;
+            this.lblPontuacao2.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPontuacao2.Location = new System.Drawing.Point(625, 349);
+            this.lblPontuacao2.Name = "lblPontuacao2";
+            this.lblPontuacao2.Size = new System.Drawing.Size(76, 86);
+            this.lblPontuacao2.TabIndex = 8;
+            this.lblPontuacao2.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(609, 316);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 26);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Jogador 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lblPontuacao2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblPontuacao1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblVez);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Velha";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVez;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button a33;
+        private System.Windows.Forms.Button a32;
+        private System.Windows.Forms.Button a31;
+        private System.Windows.Forms.Button a23;
+        private System.Windows.Forms.Button a22;
+        private System.Windows.Forms.Button a21;
+        private System.Windows.Forms.Button a13;
+        private System.Windows.Forms.Button a12;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPontuacao1;
+        private System.Windows.Forms.Label lblPontuacao2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ jogoDaVelhaArthur/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jogoDaVelhaArthur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            limparCampos();
+        }
+
+        Boolean vezJogador = true;
+        int contarVezes = 0;
+        int contarVitoria1 = 0;
+        int contarVitoria2 = 0;
+
+        void limparCampos()
+        {
+            a11.Text = "";
+            a12.Text = "";
+            a13.Text = "";
+            a21.Text = "";
+            a22.Text = "";
+            a23.Text = "";
+            a31.Text = "";
+            a32.Text = "";
+            a33.Text = "";
+
+            lblVez.Text = "Vez do Jogador 1 (X)";
+            contarVezes = 0;
+            vezJogador = true;
+        }
+        
+        void ManipularBotoes(Button botao)
+        {            
+            if (vezJogador == true)
+            {
+                if(botao.Text == "")
+                {
+                    botao.Text = "X";
+                    vezJogador = false;
+                    lblVez.Text = "Vez do Jogador 2 (O)";
+                    contarVezes = contarVezes + 1;
+                }
+            }
+            else
+            {
+                if (botao.Text == "")
+                {
+                    botao.Text = "O";
+                    vezJogador = true;
+                    lblVez.Text = "Vez do Jogador 1 (X)";
+                    contarVezes = contarVezes + 1;
+                }
+            }
+            verificarVencedor();
+        }
+
+        void verificarVencedor()
+        {
+            if(
+                a11.Text == "X" && a12.Text == "X" && a13.Text == "X" ||
+                a21.Text == "X" && a22.Text == "X" && a23.Text == "X" ||
+                a31.Text == "X" && a32.Text == "X" && a33.Text == "X" ||
+                a11.Text == "X" && a21.Text == "X" && a31.Text == "X" ||
+                a12.Text == "X" && a22.Text == "X" && a32.Text == "X" ||
+                a13.Text == "X" && a23.Text == "X" && a33.Text == "X" ||
+                a11.Text == "X" && a22.Text == "X" && a33.Text == "X" ||
+                a13.Text == "X" && a22.Text == "X" && a31.Text == "X"
+              )
+            {
+                MessageBox.Show("O JOGADOR 1 VENCEU");
+                contarVitoria1 = contarVitoria1 + 1;
+                lblPontuacao1.Text = contarVitoria1.ToString();
+                limparCampos();
+            }else if 
+                (
+                a11.Text == "O" && a12.Text == "O" && a13.Text == "O" ||
+                a21.Text == "O" && a22.Text == "O" && a23.Text == "O" ||
+                a31.Text == "O" && a32.Text == "O" && a33.Text == "O" ||
+                a11.Text == "O" && a21.Text == "O" && a31.Text == "O" ||
+                a12.Text == "O" && a22.Text == "O" && a32.Text == "O" ||
+                a13.Text == "O" && a23.Text == "O" && a33.Text == "O" ||
+                a11.Text == "O" && a22.Text == "O" && a33.Text == "O" ||
+                a13.Text == "O" && a22.Text == "O" && a31.Text == "O"
+                ) 
+            {
+                MessageBox.Show("O JOGADOR 2 VENCEU");
+                contarVitoria2 = contarVitoria2 + 1;
+                lblPontuacao2.Text = contarVitoria2.ToString();
+                limparCampos();
+            }else
+            {
+                if( contarVezes >= 9)
+                { 
+                    MessageBox.Show("JOGO DEU VELHA");
+                    limparCampos();
+                }
+            }
+        }
+
+        private void a11_Click(object sender, EventArgs e)
+        {
+            ManipularBotoes(a11);
+
+        }
+
+        private void a12_Click(object sender, EventArgs e)
+        {
+            ManipularBotoes(a12);
+
+        }
+
+        private void a13_Click(object sender, EventArgs e)
+        {
+            ManipularBotoes(a13);
+        }
+
+        private void a21_Click(object sender, EventArgs e)
+        {
+            ManipularBotoes(a21);
+        }
+
+        private void a22_Click(object sender, EventArgs e)
+        {
+            ManipularBotoes(a22);
+        }
+
+        private void a23_Click(object sender, EventArgs e)
+        {
+            ManipularBotoes(a23);
+        }
+
+        private void a31_Click(object sender, EventArgs e)
+        {
+            ManipularBotoes(a31);
+        }
+
+        private void a32_Click(object sender, EventArgs e)
+        {
+            ManipularBotoes(a32);
+        }
+
+        private void a33_Click(object sender, EventArgs e)
+        {
+            ManipularBotoes(a33);
+        }
+    }
+}
